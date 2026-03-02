64d9b8a9c80c058cafa1b569198f55bfecb7b8aa
thekiwismarthome <ksh@mccabe.kiwi>
2026-03-02 17:47:07 +1300

    build: move `recipe-manager-card.js` output from `dist` to project root

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import terser from '@rollup/plugin-terser';
 export default {
   input: 'src/recipe-manager-card.js',
   output: {
-    file: 'dist/recipe-manager-card.js',
+    file: 'recipe-manager-card.js',
     format: 'es',
     sourcemap: false
   },
